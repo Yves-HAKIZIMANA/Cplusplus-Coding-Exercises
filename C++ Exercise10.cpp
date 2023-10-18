f7594dc3f9125eb669daad0494479ec71efb05d5
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2023-10-18 16:05:42 +0200

    area and perimeter of rectangle

---
+++ C++ Exercise10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int area(int& length, int& width){
+    return width * length;
+}
+
+int perimeter(int& length, int& width){
+    return (2 * ( width + length));
+}
+
+int main(){
+    cout << "Enter the length and the width : , " ;
+    int length, width;
+    cin >> length >> width;
+    int pEremiter = perimeter(length, width);
+    int aRea  = area(length, width);
+    cout << "The area : " << aRea << " m ^ 2" << endl
+        << "The Perimeter: " << pEremiter << " m";
+}
