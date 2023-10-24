ca327f4713f1114da9665f4d70cb9fb29ac159a4
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2023-10-24 21:47:11 +0200

    Calculating the area and circumference of the circle

--- C++ Exercise12.cpp
+++ C++ Exercise12.cpp
@@ -9,11 +9,11 @@ int computeRemainder(int& first, int& second){
     return first % second;
 }
 
-int main(){
-    int first = 10;
-    int second = 3;
-    int quotient = computeQuotient(first, second);
-    int remainder = computeRemainder(first, second);
-    cout << "Quotient : " << quotient << endl
-        << "Remainder : " << remainder << endl;
-}
+//int main(){
+//    int first = 10;
+//    int second = 3;
+//    int quotient = computeQuotient(first, second);
+//    int remainder = computeRemainder(first, second);
+//    cout << "Quotient : " << quotient << endl
+//        << "Remainder : " << remainder << endl;
+//}
---
+++ C++ Exercise16.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double area(double radius){
+    return (M_PI * pow(radius, 2));
+}
+
+double circumference(double radius){
+    return (M_PI * radius * 2);
+}
+
+int main(){
+    double radius;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+    cout << "The area of the circle is : " << area(radius) << endl
+        << "The circumference of the circle is " << circumference(radius) << endl;
+}
